eaaef2afdb503627e28df035f34a24f764410c11
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2023-09-01 18:33:05 +0200

    login auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/',"pages.home");
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
