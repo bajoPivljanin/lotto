aa3f1786d5a8014f00c00de7131cbb087bc64f32
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2023-09-22 19:59:59 +0200

    Added house cut

--- app/Console/Commands/StartLotoGame.php
+++ app/Console/Commands/StartLotoGame.php
@@ -35,12 +35,18 @@ class StartLotoGame extends Command
             }
             $lotoNumbers[] = $randomNumber;
         }
+
+        $award = Tickets::getForPast7Days();
+        $houseCut = $award * env('HOUSE_CUT');
+        $award = $award - $houseCut;
+
         LottoGames::create([
             'numbers' => implode(',',$lotoNumbers),
-            'award_fund' => Tickets::getForPast7Days()
+            'award_fund' => $award
         ]);
 
         $this->output->info("Numbers that were picked: ".implode(',',$lotoNumbers));
+        $this->output->info("Award winning fund was $award and house kept $houseCut");
 
     }
 }
